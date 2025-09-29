8c0914b4cfdb190d147eb25016831d48bf451cf3
itsmenavi38 <sharma.naveen341989@gmail.com>
2025-09-29 18:10:24 +0530

    docker file updated

--- apps/sim/drizzle.config.ts
+++ apps/sim/drizzle.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'drizzle-kit'
 import { env } from './lib/env'
+import 'dotenv/config'
 
-console.log(process.env.DATABASE_URL, '<<<<<<<<<<<<')
+console.log('env.DATABASE_URL:', env.DATABASE_URL ?? '(undefined)')
+console.log('process.env.DATABASE_URL:', process.env.DATABASE_URL ?? '(undefined)')
+
 export default {
   schema: './db/schema.ts',
   out: './db/migrations',
