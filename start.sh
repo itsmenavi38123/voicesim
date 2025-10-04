85d6a123f0cc7fed1a6f3437400bbb969db2177a
itsmenavi38 <sharma.naveen341989@gmail.com>
2025-10-04 12:02:48 +0530

    security file updated

--- start.sh
+++
@@ -1,4 +0,0 @@
-  GNU nano 7.2                                                       start.sh                                                                 
-#!/bin/bash
-cd /home/azureuser/voicesim/apps/sim
-bun run dev:full
